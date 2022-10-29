ffac9f47bf39ec8ebdbfe4bec135e587ac23f90f
dimasadh <dimasadh24@gmail.com>
2022-10-29 15:48:11 +0700

    add logic only can see post from followed user

--- InstaWeb/app/Http/Controllers/PostsController.php
+++ InstaWeb/app/Http/Controllers/PostsController.php
@@ -20,7 +20,7 @@ class PostsController extends Controller
 
     public function index()
     {
-        $users = auth()->user()->pluck('id');
+        $users = auth()->user()->following()->pluck('profiles.user_id');
         
         $posts = Post::whereIn('user_id', $users)->with('user')->latest()->paginate(5);
         
@@ -41,7 +41,7 @@ class PostsController extends Controller
 
         $imagePath = request('image') -> store('uploads', 'public');
 
-        $image = Image::make($request->file('image'))->fit(1200,1200);
+        $image = Image::make($request->file('image'))->fit(1200, 1200);
         $image->save(public_path("storage/{$imagePath}"));
         Post::create([
             'caption' => $data['caption'],
--- InstaWeb/resources/views/posts/show.blade.php
+++ InstaWeb/resources/views/posts/show.blade.php
@@ -38,14 +38,21 @@
                 @foreach($post->comments as $comment)
                 <div class="comment-widgets m-b-20">
                     <div class="d-flex flex-row comment-row">
-                        <div class="p-2"><span class="round"><img src="{{ $comment->user->profile->profileImage() }}" alt="user" width="50"></span>
-                    </div>
+                        <div class="p-2">
+                            <span class="round">
+                                <img src="{{ $comment->user->profile->profileImage() }}" alt="user" width="50">
+                            </span>
+                        </div>
                         <div class="comment-text w-100">
                             <h5>{{$comment->user->username}}</h5>
                             <div class="comment-footer">
                                 <span class="date">{{$comment->created_at}}</span> 
                                 @can('delete', $comment)
-                                    <span><a href="/c/{{ $comment->id }}/d" data-abc="true"><i class="fa fa-trash" aria-hidden="true"></i></a><span>
+                                    <span>
+                                        <a href="/c/{{ $comment->id }}/d" data-abc="true">
+                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                        </a>
+                                    <span>
                                 @endcan
                             </div>
                             <p class="m-b-5 m-t-10">{{$comment->comment}}</p>
@@ -61,7 +68,7 @@
         <!-- add comment section -->
         <form action="/p/{{ $post->id }}/c" enctype="multipart/form-data" method="post" id="comment-form" class="col-md-12 pt-5 form-block">
         @csrf
-            <div class="col-xs-12">									
+            <div class="col-xs-12">
                 <div class="form-group">
                     <textarea name="comment" class="form-input" required="" form="comment-form" placeholder="Your comment"></textarea>
                 </div>
